1c6f277ab44d76d31797d39e8fcbb267159e47ee
localcommunityvendorsllc <cindy.onyekwelu@gmail.com>
2025-01-11 17:18:24 -0600

    Counter

--- README.md
+++
@@ -1,1 +0,0 @@
-# Review Git commands, linux commands and Markdown commands within this READ.ME Repo for training purposes. This is my practice Repo!
--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-//Represents all of the colors is the name of this array of values it will cycle through the array of colors 
-const colors = ["blue" , "red" , "yellow" , "green" , "rgba(133, 122, 200)", "#f15025"];
+//set initial count
+let count = 0;
 
-const bin = document.getElementById("btn");
-const color = document.querySelector(".color");
+// Select Value and Buttons, #value is looking for the id value from my span in the html doc
+const value = document.querySelector('#value');
+//btns is storing all 3 btn classes together from the html page, classes labled like .btn for document queryselector or 1 or queryselectoAll for many
+const btns = document.querySelectorAll(".btn");
 
-btn.addEventListener('click' , function(){
-    // Get random number between 0 - 5 colors [5]
-    //Assigning name of action to funtion name.
-    const randomNumber = getRandomNumber();
-    //console output helps when using "Dev Inspect Tools"
-    console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
+//open dev inspect tools and open console.
+// you will see it's a node list that would need to be transfored into an array.
+//console.log(btns);
+
+//(btn) can be named anything here since it's a parameter we created
+//For Each() method is called on our btns created up there calling all btn calls like .btn
+//Creating a callback function to access each and every item within my list all the buttons we plan to loop through
+btns.forEach(function (btn) {
+    //passing the callback function to knew which button was clicked
+    btn.addEventListener('click' , function(e){
+        //the e is us accessing the event object has a property of current target being clicked from the call back function
+        //Figure out how .classList effect the currentTarget
+        const styles = e.currentTarget.classList;
+        //styles holds the value being checked for eah btn class of 'decrease, reset, or increase')
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('increase')){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count === 0){
+            value.style.color = "#222";
+        }
+        //Using the const #value placeholder
+        value.textContent = count;
+    })
 });
-
-//Creating a function that gets me random numbers that activates the Math method calling random.
-//Math calling floor method (rounds a number DOWN to the nearest integer) to contain the random number times the length of the array helped form soild number (fixed values) without decimals 
-function getRandomNumber() {
-    return Math.floor( Math.random() * colors.length);
-}
